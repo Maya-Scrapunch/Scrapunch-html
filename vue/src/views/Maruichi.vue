dd1ba4538b0df4d9f26175187bca5d9410848b2d
OsawaShiho <totoro.10106s@yahoo.ne.jp>
2023-09-10 00:16:44 +0900

    丸一紹介文作成

--- vue/src/views/Maruichi.vue
+++ vue/src/views/Maruichi.vue
@@ -3,23 +3,41 @@
 
 <template>
   <main>
-    <div>
-        <p>丸一</p>
-    </div>
-    <div>
+  <v-container>
+      <v-card
+      width="900"
+      title="丸一"
+      subtitle="湖南のそば粉を用いた手打ちのおそば屋さん">
         <v-container>
-        <v-row>
-            <v-col>
-            <img src='../assets/img/maruichi/outside.jpg' alt="外部の画像" width="200" height="250">
-            </v-col>
-            <v-col>
-            <img src='../assets/img/maruichi/inside.jpg' alt="外部の画像" width="250" height="200">
-            </v-col>
-            <v-col>
-            <img src='../assets/img/maruichi/tempurasoba.jpg' alt="外部の画像" width="250" height="200">
-            </v-col>
-        </v-row>
+            <v-card width="850">
+                <v-row>
+                    <v-col>
+                        <img src='../assets/img/maruichi/outside.jpg' alt="外部の画像" width="200" height="250">
+                    </v-col>
+                    <v-col>
+                        <img src='../assets/img/maruichi/inside.jpg' alt="外部の画像" width="250" height="200">
+                    </v-col>
+                    <v-col>
+                        <img src='../assets/img/maruichi/tempurasoba.jpg' alt="外部の画像" width="250" height="200">
+                    </v-col>
+                </v-row>
+            <v-card
+            title="矢沢永吉ファンの店主がつくる絶品そば"
+            >
+            <v-container>
+            <v-card width="800">
+            福島県は全国有数のそば畑の規模を誇る県です。<br>
+            蕎麦は、猪苗代湖周辺の機構では標高500～700mが適地といわれ、湖南の蕎麦の風味が脚光を浴びています。<br>
+            その蕎麦粉を使った手打ち蕎麦をいただけるのがこちらのお店、「MARU壱」。<br>
+            地元民なら誰もが知っている有名店です。<br>
+            MARU壱の店主は、大の矢沢永吉ファンで、気分が良いと矢沢永吉の真似をしてくれるそうです。<br>
+            湖南地方の蕎麦を味わいたい方、地元住民との交流を楽しみたい方は必見です！
+            </v-card>
+            </v-container>
+            </v-card>
+            </v-card>
         </v-container>
-    </div>
+        </v-card>
+    </v-container>
   </main>
 </template>
